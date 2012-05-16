928c316e88ea3fdc2e620d2d7b3082deac5ad528
J. Ky Marsh <jkymarsh@gmail.com>
2012-05-16 00:47:21 -0500

    Added lightweight plugin pattern and test HTML for tables

--- js/bedhed.js
+++ js/bedhed.js
@@ -1,7 +1,58 @@
-/*
-    Bedhed.js - A jQuery plugin for better headers for particularly long tables
+/*!
+ * Bedhed.js - A jQuery plugin for better headers for particularly long tables
+ *
+ * J. Ky Marsh, jkymarsh@gmail.com
+ * Copyright (c) 2012
+ */
 
-    Author: J. Ky Marsh, jkymarsh@gmail.com
+;
 
-    Copyright (c) 2012
-*/
+(function($, window, document, undefined) {
+
+    // Create the defaults and set up the constructor for the plugin itself
+    var pluginName = "bedhed",
+        defaults = {
+            mode: "repeater",
+            period: 5
+        },
+        Plugin = function(element, options) {
+            this.element = element;
+            this.defaults = defaults;
+            this.name = pluginName;
+            this.options = $.extend({}, defaults, options);
+
+            console.log(this);
+
+            this.init();
+        };
+
+    // Initialization method for the plugin fires after setup is complete
+    Plugin.prototype.init = function() {
+        var $element = $(this.element),
+            options = this.options,
+            thContent;
+
+        console.log($element);
+        console.log(this.defaults);
+        console.log(this.name);
+        console.log(options);
+
+        $element.find("th").each(function() {
+            thContent = $(this).html();
+
+            console.log(thContent);
+        });
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                new Plugin(this, options));
+            }
+        });
+    }
+
+})(jQuery, window, document);
